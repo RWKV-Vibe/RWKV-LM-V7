051c152983b4dc2853757d22cbbe9a7084cc475c
zhiyuan li <uniartisan2017@gmail.com>
2025-07-07 16:49:55 +0800

    [Refrac] Prepare for multi-backend

--- src/model.py
+++ src/model.py
@@ -15,7 +15,8 @@ from rwkvfla.modules.token_shift import token_shift
 from rwkvfla.ops.rwkv7.fused_addcmul import fused_addcmul_rwkv7
 from rwkvfla.ops.rwkv7.fused_k_update import fused_k_rwkv7
 from torch.nn import functional as F
-from torch.utils.cpp_extension import load
+
+from src.ops.cuda.wkv7 import RUN_CUDA_RWKV7g
 
 if importlib.util.find_spec("deepspeed"):
     import deepspeed
@@ -31,93 +32,9 @@ def __nop(ob):
     return ob
 
 
-ROCm_flag = torch.version.hip is not None
 CompileFunction = __nop
 if os.environ["RWKV_COMPILE_ON"] == "1":
     CompileFunction = torch.compile
-
-
-#################################################################
-# CUDA Kernel
-#################################################################
-
-
-HEAD_SIZE = int(os.environ["RWKV_HEAD_SIZE"])
-
-if "x070" in os.environ["RWKV_MY_TESTING"]:
-    CHUNK_LEN = 16
-
-    if ROCm_flag is True:
-        flags = [
-            f"-D_C_={HEAD_SIZE}",
-            f"-D_CHUNK_LEN_={CHUNK_LEN}",
-            "-xhip",
-            "-fopenmp",
-            "-ffast-math",
-            "-O3",
-            "-munsafe-fp-atomics",
-        ]
-        load(
-            name="wind_backstepping_hip",
-            sources=["cuda/wkv7_hip.hip", "cuda/wkv7_op.hip"],
-            is_python_module=False,
-            verbose=True,
-            extra_cuda_cflags=flags,
-        )
-    else:
-        flags = [
-            "-res-usage",
-            f"-D_C_={HEAD_SIZE}",
-            f"-D_CHUNK_LEN_={CHUNK_LEN}",
-            "--use_fast_math",
-            "-O3",
-            "-Xptxas -O3",
-            "--extra-device-vectorization",
-        ]
-        load(
-            name="wind_backstepping",
-            sources=["cuda/wkv7_cuda.cu", "cuda/wkv7_op.cpp"],
-            is_python_module=False,
-            verbose=True,
-            extra_cuda_cflags=flags,
-        )
-
-    class WindBackstepping(torch.autograd.Function):
-        @staticmethod
-        def forward(ctx, w, q, k, v, z, b):
-            B, T, H, C = w.shape
-            assert T % CHUNK_LEN == 0
-            assert all(i.dtype == torch.bfloat16 for i in [w, q, k, v, z, b])
-            assert all(i.is_contiguous() for i in [w, q, k, v, z, b])
-            y = torch.empty_like(v)
-            s = torch.empty(
-                B, H, T // CHUNK_LEN, C, C, dtype=torch.float32, device=w.device
-            )
-            sa = torch.empty(B, T, H, C, dtype=torch.float32, device=w.device)
-            torch.ops.wind_backstepping.forward(w, q, k, v, z, b, y, s, sa)
-            ctx.save_for_backward(w, q, k, v, z, b, s, sa)
-            return y
-
-        @staticmethod
-        def backward(ctx, dy):
-            assert all(i.dtype == torch.bfloat16 for i in [dy])
-            assert all(i.is_contiguous() for i in [dy])
-            w, q, k, v, z, b, s, sa = ctx.saved_tensors
-            dw, dq, dk, dv, dz, db = [torch.empty_like(x) for x in [
-                w, q, k, v, z, b]]
-            torch.ops.wind_backstepping.backward(
-                w, q, k, v, z, b, dy, s, sa, dw, dq, dk, dv, dz, db
-            )
-            return dw, dq, dk, dv, dz, db
-
-    def RUN_CUDA_RWKV7g(q, w, k, v, a, b):
-        B, T, HC = q.shape
-        q, w, k, v, a, b = [i.view(B, T, HC // 64, 64)
-                            for i in [q, w, k, v, a, b]]
-        return WindBackstepping.apply(w, q, k, v, a, b).view(B, T, HC)
-
-
-#################################################################
 
 
 class RWKV_Tmix_x070(nn.Module):
